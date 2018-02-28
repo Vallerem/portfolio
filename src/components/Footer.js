bd254156950771d1cb7f5a03cbd98344de48abb8
Manuel Valle <vallerem17@gmail.com>
2018-02-28 18:48:12 +0100

    links added in header section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-linkedin"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/manuel-amaro-valle" className="icon fa-linkedin"><span className="label">Linkedin</span></a></li>
+                        <li><a href="https://github.com/Vallerem" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="mailto:manuelvalle@vallerem.com" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
                         <li>&copy; 2018 Manuel Amaro Valle Redondo</li>
