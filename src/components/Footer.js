c4ea8467cda576e011ea95890eb25b51bf5e2068
Manuel Valle <vallerem17@gmail.com>
2018-02-28 17:13:28 +0100

    Forst commit

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div id="footer">
+                <div className="inner">
+                    <ul className="icons">
+                        <li><a href="#" className="icon fa-linkedin"><span className="label">Twitter</span></a></li>
+                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                    </ul>
+                    <ul className="copyright">
+                        <li>&copy; 2018 Manuel Amaro Valle Redondo</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer
